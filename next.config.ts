f5ff1ccf3ab78fe616205c89a92af0f61ed73d3d
Fabian <116721837+Fabian-Petersen@users.noreply.github.com>
2025-04-09 15:33:19 +0200

    updated ChakraUI component

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  output: "export",
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+    remotePatterns: [],
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
