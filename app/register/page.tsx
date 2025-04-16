322f14a36f3111c1175bb6dae7f66a8159c82ffe
Fabian <116721837+Fabian-Petersen@users.noreply.github.com>
2025-04-16 22:25:16 +0200

    add register page

---
+++ app/register/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import RegisterForm from "../components/register/RegisterForn";
+
+export default function RegisterPage() {
+  return (
+    <>
+      <RegisterForm />
+    </>
+  );
+}
--- app/schemas/index.ts
+++ app/schemas/index.ts
@@ -1,5 +1,6 @@
 import { jobSchema } from "./jobSchema";
 import { projectSchema } from "./projectSchema";
 import { loginSchema } from "./loginSchema";
+import { registerSchema } from "./registerSchema";
 
-export { jobSchema, projectSchema, loginSchema };
+export { jobSchema, projectSchema, loginSchema, registerSchema };
